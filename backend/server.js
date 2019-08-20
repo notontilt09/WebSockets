7e1710adc1713cb9da141692935988197ef25755
Daniel Weinman <namniewd@gmail.com>
2019-08-20 08:28:42 -0400

    cleaned up server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3030 });
 
-let colors = ['red', 'blue']
-
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
